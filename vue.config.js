ca858c01ae3cba2dd93d0aefa2da7dcea05a9149
Chenrt <2926777985@qq.com>
2022-11-14 18:33:06 +0800

    added action

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_VERSION === 'BuildAction' ? './' : '/',
   productionSourceMap: false,
   transpileDependencies: true
 });
